dde8999846db8c5dcd0a05ef761080f965b87a0a
Pavol Vargovčík <pavol.vargovcik@gmail.com>
2025-01-08 14:41:11 +0100

    holder+onion wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-use std::collections::{HashMap, HashSet};
 use std::mem::MaybeUninit;
 use std::ptr::addr_of_mut;
-use serde_json::Value;
+use hashbrown::{HashMap, HashSet};
 
 pub mod lock;
 pub mod command;
@@ -16,233 +15,143 @@ pub mod keyval_nfa;
 pub mod char_runner;
 pub mod borrow_lock;
 pub mod blob;
+pub mod holder;
 
-// pub mod parser_to_simulator;
+use crate::lock::LockSelector;
 
-use crate::lock::{LockSelector, Lock};
-use crate::command::{CommandBox, CommandTarget};
-
-pub struct Onion<S: LockSelector> {
-    parent: Option<S::Lock<Onion<S>>>,
-    data: HashMap<String, Value>,
+pub struct Onion<'a, S: LockSelector + 'a> {
+    parent: S::MaybeLock<'a, Self>,
+    children: S::Holder<'a, Self>,
+    data: HashMap<&'a [u8], &'a [u8]>,
 }
 
-impl<S: LockSelector> Onion<S>
+impl<'a, S: LockSelector + 'a> Onion<'a, S>
 {
     pub fn new() -> Self {
         Onion {
-            parent: None,
+            parent: S::empty(),
+            children: S::new_holder(),
             data: HashMap::new(),
         }
     }
 
-    pub fn share(self) -> S::Lock<Self> {
-        <S::Lock<Self>>::from(self)
-    }
-
-    pub fn new_level(parent: S::Lock<Self>) -> Self {
-        Onion {
-            parent: Some(parent.clone()),
+    pub fn make_child(slf: S::Lock<'a, Self>) -> S::Lock<'a, Self> {
+        let child = Onion {
+            parent: S::to_maybe_lock(&slf),
+            children: S::new_holder(),
             data: HashMap::new(),
-        }
+        };
+        let children = &mut S::borrow_mut(&slf).children;
+        S::add_holder(children, child)
     }
 
-    pub fn get_rec(&self, key: &str) -> Option<Value> {
-        if let Some(value) = self.data.get(key) {
-            return Some(value.clone());
-        }
-        let parent_ref = self.parent.as_ref()?;
-        let parent_onion = parent_ref.borrow();
-        parent_onion.get_rec(key)
-    }
+    // pub fn get_rec(&self, key: &[u8]) -> Option<&'a [u8]> {
+    //     if let Some(value) = self.data.get(key) {
+    //         return Some(value);
+    //     }
+    //     let parent_onion = S::borrow(&self.parent)?;
+    //     parent_onion.get_rec(key)
+    // }
 
-    pub fn get(&self, key: &str) -> Option<Value> {
-        if let Some(value) = self.data.get(key) {
-            return Some(value.clone());
-        }
+    // pub fn get(&self, key: &[u8]) -> Option<&'a [u8]> {
+    //     if let Some(value) = self.data.get(key) {
+    //         return Some(value);
+    //     }
 
-        let mut parent = self.parent.clone();
-        loop {
-            let parent_ref = parent.take()?;
-            let parent_onion = parent_ref.borrow();
-            if let Some(value) = parent_onion.data.get(key) {
-                return Some(value.clone());
-            }
-            parent = parent_onion.parent.clone();
-        }
-    }
+    //     let mut parent = &self.parent;
+    //     let mut parent_onion = S::borrow(parent)?;
+    //     loop {
+    //         if let Some(value) = parent_onion.data.get(key) {
+    //             return Some(value);
+    //         }
+    //         parent = &parent_onion.parent;
+    //         parent_onion = S::borrow(parent)?;
+    //     }
+    // }
 
-    pub fn set(&mut self, key: String, value: Value) {
+    pub fn set(&mut self, key: &'a [u8], value: &'a [u8]) {
         self.data.insert(key, value);
     }
 
-    pub fn iter(&self) -> OnionIter<S> {
-        OnionIter::new(self)
-    }
+    // pub fn iter(&self) -> OnionIter<S> {
+    //     OnionIter::new(self)
+    // }
 }
 
-pub struct OnionIter<S: LockSelector> {
-    parent: Option<S::Lock<Onion<S>>>,
-    visited: HashSet<String>,
-    items: Vec<(String, Value)>,
-}
+// pub struct OnionIter<S: LockSelector> {
+//     parent: S::Lock<Onion<S>>,
+//     children: Vec<S::Holder<Onion<S>>>,
+//     visited: HashSet<String>,
+//     items: Vec<(String, Value)>,
+// }
+// 
+// impl <S: LockSelector> OnionIter<S> {
+//     pub fn new(onion: &Onion<S>) -> Self {
+//         OnionIter {
+//             parent: onion.parent.clone(),
+//             visited: HashSet::new(),
+//             items: onion.data.iter().map(|(key, val)| (key.clone(), val.clone())).collect(),
+//         }
+//     }
+// }
 
-impl <S: LockSelector> OnionIter<S> {
-    pub fn new(onion: &Onion<S>) -> Self {
-        OnionIter {
-            parent: onion.parent.clone(),
-            visited: HashSet::new(),
-            items: onion.data.iter().map(|(key, val)| (key.clone(), val.clone())).collect(),
-        }
-    }
-}
+// impl <S: LockSelector> Iterator for OnionIter<S>
+// {
+//     type Item = (String, Value);
+// 
+//     fn next(&mut self) -> Option<Self::Item> {
+//         loop {
+//             while let Some((key, value)) = self.items.pop() {
+//                 if self.visited.insert(key.clone()) {
+//                     return Some((key, value));
+//                 }
+//             }
+// 
+//             let parent_ref = self.parent.take()?;
+//             let parent_onion = parent_ref.borrow();
+// 
+//             self.items.extend(parent_onion.data.iter().map(|(key, val)| (key.clone(), val.clone())));
+//             self.parent = parent_onion.parent.clone();
+//         }
+//     }
+// }
 
-impl <S: LockSelector> Iterator for OnionIter<S>
-{
-    type Item = (String, Value);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            while let Some((key, value)) = self.items.pop() {
-                if self.visited.insert(key.clone()) {
-                    return Some((key, value));
-                }
-            }
-
-            let parent_ref = self.parent.take()?;
-            let parent_onion = parent_ref.borrow();
-
-            self.items.extend(parent_onion.data.iter().map(|(key, val)| (key.clone(), val.clone())));
-            self.parent = parent_onion.parent.clone();
-        }
-    }
-}
-
-
-pub trait Command {}
-
-pub struct Configmaton<S: LockSelector> {
-    onion: S::Lock<Onion<S>>,
-    commands: HashSet<CommandBox<Self>>,
-    stepping: bool,
-}
-
-impl <S: LockSelector> CommandTarget for Configmaton<S> {
-    fn custom_fn(&mut self, value: &Value) {
-        println!("CustomFn {}", value);
-    }
-
-    fn set(&mut self, key: &str, value: Value) {
-        if self.stepping {
-            self.onion.borrow_mut().set(key.to_string(), value);
-        } else {
-            self.stepping = true;
-
-            let commands1 = self.on_read(unsafe { std::ptr::read(&self.commands) }, key, &value);
-            let commands2 = self.post_read(commands1);
-            self.onion.borrow_mut().set(key.to_string(), value);
-            unsafe { std::ptr::write(&mut self.commands, commands2) }
-            self.stepping = false;
-        }
-    }
-
-    fn get(&self, key: &str) -> Option<Value> {
-        self.onion.borrow().get(key)
-    }
-}
-
-impl <S: LockSelector> Configmaton<S> {
-    pub fn new(commands: HashSet<CommandBox<Self>>) -> Self {
-        let mut uresult: MaybeUninit<Configmaton<S>> = MaybeUninit::uninit();
-        let presult = uresult.as_mut_ptr();
-        unsafe { addr_of_mut!((*presult).onion).write(Onion::<S>::new().share()) };
-        unsafe { addr_of_mut!((*presult).stepping).write(true) };
-        let commands1 = unsafe { (*presult).post_read(commands) };
-        unsafe { addr_of_mut!((*presult).commands).write(commands1) };
-        let mut result = unsafe { uresult.assume_init() };
-        result.stepping = false;
-        result
-    }
-
-    pub fn new_level(self) -> Self {
-        Configmaton {
-            onion: Onion::<S>::new_level(self.onion).share(),
-            commands: self.commands.clone(),
-            stepping: false,
-        }
-    }
-
-    pub fn get(&self, key: &str) -> Option<Value> {
-        self.onion.borrow().get(key)
-    }
-
-    fn post_read(&mut self, mut commands: HashSet<CommandBox<Self>>)
-        -> HashSet<CommandBox<Self>>
-    {
-        let mut visited = HashSet::new();
-        let mut new_commands = HashSet::new();
-
-        while let Some(state) = commands.iter().next().cloned() {
-            commands.remove(&state);
-            if !visited.insert(state) {
-                continue;
-            }
-            unsafe { &*state.inner }.execute_post(&mut commands, &mut new_commands, self);
-        }
-
-        return new_commands;
-    }
-
-    fn on_read(&mut self, mut commands: HashSet<CommandBox<Self>>, key: &str, value: &Value)
-        -> HashSet<CommandBox<Self>>
-    {
-        let mut visited = HashSet::new();
-        let mut new_commands = HashSet::new();
-
-        while let Some(state) = commands.iter().next().cloned() {
-            commands.remove(&state);
-            if !visited.insert(state) {
-                continue;
-            }
-            unsafe { &*state.inner }.execute(key, value, &mut commands, &mut new_commands, self);
-        }
-
-        return new_commands;
-    }
-}
+// pub struct Configmaton<'a, S: LockSelector> {
+//     onion: S::Lock<'a, Onion<'a, S>>,
+// }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use serde_json::Number;
-    use crate::lock::RcRefCellSelector;
-
-    #[test]
-    fn onion_works() {
-        let mut onion1 = Onion::<RcRefCellSelector>::new();
-        onion1.set("a".to_string(), Value::Number(Number::from(1)));
-        onion1.set("b".to_string(), Value::Number(Number::from(2)));
-        onion1.set("a".to_string(), Value::Number(Number::from(3)));
-        assert_eq!(onion1.get("a"), Some(Value::Number(Number::from(3))));
-        assert_eq!(onion1.get("b"), Some(Value::Number(Number::from(2))));
-        assert_eq!(onion1.get("c"), None);
-
-        let onion1 = onion1.share();
-        let mut onion2 = Onion::<RcRefCellSelector>::new_level(onion1.clone());
-        onion2.set("b".to_string(), Value::Number(Number::from(4)));
-        onion2.set("c".to_string(), Value::Number(Number::from(5)));
-        assert_eq!(onion2.get("a"), Some(Value::Number(Number::from(3))));
-        assert_eq!(onion2.get("b"), Some(Value::Number(Number::from(4))));
-        assert_eq!(onion2.get("c"), Some(Value::Number(Number::from(5))));
-        assert_eq!(onion2.get("d"), None);
-
-        assert_eq!(onion1.borrow().get("a"), Some(Value::Number(Number::from(3))));
-        assert_eq!(onion1.borrow().get("b"), Some(Value::Number(Number::from(2))));
-        assert_eq!(onion1.borrow().get("c"), None);
-
-        onion1.borrow_mut().set("a".to_string(), Value::Number(Number::from(6)));
-        assert_eq!(onion1.borrow().get("a"), Some(Value::Number(Number::from(6))));
-        assert_eq!(onion2.get("a"), Some(Value::Number(Number::from(6))));
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use serde_json::Number;
+//     use crate::lock::RcRefCellSelector;
+// 
+//     #[test]
+//     fn onion_works() {
+//         let mut onion1 = Onion::<RcRefCellSelector>::new();
+//         onion1.set("a".to_string(), Value::Number(Number::from(1)));
+//         onion1.set("b".to_string(), Value::Number(Number::from(2)));
+//         onion1.set("a".to_string(), Value::Number(Number::from(3)));
+//         assert_eq!(onion1.get("a"), Some(Value::Number(Number::from(3))));
+//         assert_eq!(onion1.get("b"), Some(Value::Number(Number::from(2))));
+//         assert_eq!(onion1.get("c"), None);
+// 
+//         let onion1 = onion1.share();
+//         let mut onion2 = Onion::<RcRefCellSelector>::new_level(onion1.clone());
+//         onion2.set("b".to_string(), Value::Number(Number::from(4)));
+//         onion2.set("c".to_string(), Value::Number(Number::from(5)));
+//         assert_eq!(onion2.get("a"), Some(Value::Number(Number::from(3))));
+//         assert_eq!(onion2.get("b"), Some(Value::Number(Number::from(4))));
+//         assert_eq!(onion2.get("c"), Some(Value::Number(Number::from(5))));
+//         assert_eq!(onion2.get("d"), None);
+// 
+//         assert_eq!(onion1.borrow().get("a"), Some(Value::Number(Number::from(3))));
+//         assert_eq!(onion1.borrow().get("b"), Some(Value::Number(Number::from(2))));
+//         assert_eq!(onion1.borrow().get("c"), None);
+// 
+//         onion1.borrow_mut().set("a".to_string(), Value::Number(Number::from(6)));
+//         assert_eq!(onion1.borrow().get("a"), Some(Value::Number(Number::from(6))));
+//         assert_eq!(onion2.get("a"), Some(Value::Number(Number::from(6))));
+//     }
+// }
--- src/lock.rs
+++ src/lock.rs
@@ -1,242 +1,270 @@
-use std::cell::{RefCell, Ref, RefMut};
-use std::hash::Hash;
-use std::mem::MaybeUninit;
+use std::cell::{Ref, RefMut};
 use std::ops::{Deref, DerefMut};
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::{Mutex, MutexGuard};
+
+use crate::holder::{Holder, TimedRefCell};
 
 pub trait LockSuper {
     type Guard<'a, X: 'a>: Deref<Target = X>;
     type GuardMut<'a, X: 'a>: DerefMut<Target = X>;
 }
 
-pub trait Lock<T>: LockSuper + Clone + From<T> + Hash + Eq + std::fmt::Debug {
-    fn borrow(&self) -> Self::Guard<'_, T>;
-    fn borrow_mut(&self) -> Self::GuardMut<'_, T>;
+pub trait LockSelector: LockSuper {
+    type MaybeLock<'a, T: 'a>: Copy;
+    type Lock<'a, T: 'a>: Copy;
+    type Holder<'a, T: 'a>;
+
+    fn from_maybe_lock<'a, T>(lock: &Self::MaybeLock<'a, T>) -> Option<Self::Lock<'a, T>>;
+    fn to_maybe_lock<'a, T>(lock: &Self::Lock<'a, T>) -> Self::MaybeLock<'a, T>;
+
+    fn maybe_borrow<'a, T>(lock: &Self::MaybeLock<'a, T>) -> Option<Self::Guard<'a, T>>;
+    fn maybe_borrow_mut<'a, T>(lock: &Self::MaybeLock<'a, T>) -> Option<Self::GuardMut<'a, T>>;
+    fn borrow<'a, T>(lock: &Self::Lock<'a, T>) -> Self::Guard<'a, T>;
+    fn borrow_mut<'b, 'a, T>(lock: &'b Self::Lock<'a, T>) -> Self::GuardMut<'b, T>;
+
+    fn new_holder<'a, T>() -> Self::Holder<'a, T>;
+    fn empty<'a, T>() -> Self::MaybeLock<'a, T>;
+    fn add_holder<'b, 'a, T>(holder: &'b mut Self::Holder<'a, T>, value: T) -> Self::Lock<'a, T>;
 }
 
-pub trait LockSelector {
-    type Lock<T>: Lock<T>;
-    type Holder<T>;
+type RefCellLock<'a, T> = Option<&'a TimedRefCell<'a, T>>;
+pub struct RefCellSelector;
 
-    fn new<T>(x: T) -> Self::Holder<T>;
-    fn refer<T>(x: &mut Self::Holder<T>) -> Self::Lock<T>;
-    fn borrow_mut_holder<T>(x: &mut Self::Holder<T>)
-        -> <Self::Lock<T> as LockSuper>::GuardMut<'_, T>;
-    unsafe fn refer_uninit<T>(x: &mut Self::Holder<MaybeUninit<T>>) -> Self::Lock<T>;
-}
-
-pub struct RcRefCellSelector {}
-impl LockSelector for RcRefCellSelector {
-    type Lock<T> = RcRefCell<T>;
-    type Holder<T> = RcRefCell<T>;
-
-    fn new<T>(x: T) -> Self::Holder<T> {
-        RcRefCell(std::rc::Rc::new(RefCell::new(x)))
-    }
-
-    fn refer<T>(x: &mut Self::Holder<T>) -> Self::Lock<T> {
-        x.clone()
-    }
-
-    unsafe fn refer_uninit<T>(x: &mut Self::Holder<MaybeUninit<T>>) -> Self::Lock<T> {
-        std::mem::transmute::<_, Self::Lock<T>>(x).clone()
-    }
-
-    fn borrow_mut_holder<T>(x: &mut Self::Holder<T>) -> RefMut<'_, T> {
-        x.0.borrow_mut()
-    }
-}
-
-pub struct RcRefCell<T>(std::rc::Rc<RefCell<T>>);
-
-impl<T> std::fmt::Debug for RcRefCell<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "RcRefCell({:?})", self.0.as_ptr())
-    }
-}
-
-impl<T> std::hash::Hash for RcRefCell<T> {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        std::ptr::hash(&*self.0, state);
-    }
-}
-
-impl<T> PartialEq for RcRefCell<T> {
-    fn eq(&self, other: &Self) -> bool {
-        std::ptr::eq(&*self.0, &*other.0)
-    }
-}
-
-impl<T> Eq for RcRefCell<T> { }
-
-impl<T> Clone for RcRefCell<T> {
-    fn clone(&self) -> Self {
-        RcRefCell(self.0.clone())
-    }
-}
-
-impl<T> From<T> for RcRefCell<T> {
-    fn from(x: T) -> Self {
-        RcRefCell(std::rc::Rc::new(RefCell::new(x)))
-    }
-}
-
-impl<T> LockSuper for RcRefCell<T> {
+impl LockSuper for RefCellSelector {
     type Guard<'a, X: 'a> = Ref<'a, X>;
     type GuardMut<'a, X: 'a> = RefMut<'a, X>;
 }
 
-impl<T> Lock<T> for RcRefCell<T> {
-    fn borrow(&self) -> Ref<'_, T> {
-        self.0.borrow()
+impl LockSelector for RefCellSelector {
+    type MaybeLock<'a, T: 'a> = RefCellLock<'a, T>;
+    type Lock<'a, T: 'a> = &'a TimedRefCell<'a, T>;
+    type Holder<'a, T: 'a> = Holder<'a, T>;
+
+    fn from_maybe_lock<'a, T>(lock: &Self::MaybeLock<'a, T>) -> Option<Self::Lock<'a, T>> {
+        *lock
     }
 
-    fn borrow_mut(&self) -> RefMut<'_, T> {
-        self.0.borrow_mut()
-    }
-}
-
-pub struct ArcMutex<T>(Arc<Mutex<T>>);
-
-pub struct ArcMutexSelector {}
-impl LockSelector for ArcMutexSelector {
-    type Lock<T> = ArcMutex<T>;
-    type Holder<T> = ArcMutex<T>;
-
-    fn new<T>(x: T) -> Self::Holder<T> {
-        ArcMutex(Arc::new(Mutex::new(x)))
+    fn to_maybe_lock<'a, T>(lock: &Self::Lock<'a, T>) -> Self::MaybeLock<'a, T> {
+        Some(lock)
     }
 
-    fn refer<T>(x: &mut Self::Holder<T>) -> Self::Lock<T> {
-        x.clone()
+    fn maybe_borrow<'a, T>(lock: &Self::MaybeLock<'a, T>) -> Option<Self::Guard<'a, T>> {
+        lock.map(|refcell| refcell.borrow())
     }
 
-    unsafe fn refer_uninit<T>(x: &mut Self::Holder<MaybeUninit<T>>) -> Self::Lock<T> {
-        std::mem::transmute::<_, Self::Lock<T>>(x).clone()
+    fn maybe_borrow_mut<'a, T>(lock: &Self::MaybeLock<'a, T>) -> Option<Self::GuardMut<'a, T>> {
+        lock.map(|refcell| refcell.borrow_mut())
     }
 
-    fn borrow_mut_holder<T>(x: &mut Self::Holder<T>) -> MutexGuard<'_, T> {
-        x.0.lock().unwrap()
+    fn borrow<'a, T>(lock: &Self::Lock<'a, T>) -> Self::Guard<'a, T> {
+        lock.borrow()
     }
-}
 
-impl<T> From<T> for ArcMutex<T> {
-    fn from(x: T) -> Self {
-        ArcMutex(Arc::new(Mutex::new(x)))
+    fn borrow_mut<'b, 'a, T>(lock: &'b Self::Lock<'a, T>) -> Self::GuardMut<'b, T> {
+        lock.borrow_mut()
     }
-}
 
-impl<T> Clone for ArcMutex<T> {
-    fn clone(&self) -> Self {
-        ArcMutex(self.0.clone())
+    fn new_holder<'a, T>() -> Self::Holder<'a, T> {
+        Holder::new()
+    }
+
+    fn empty<'a, T>() -> Self::MaybeLock<'a, T> {
+        None
+    }
+
+    fn add_holder<'b, 'a, T>(holder: &'b mut Self::Holder<'a, T>, value: T) -> Self::Lock<'a, T> {
+        holder.add(value)
     }
 }
 
-impl<T> LockSuper for ArcMutex<T> {
-    type Guard<'a, X: 'a> = MutexGuard<'a, X>;
-    type GuardMut<'a, X: 'a> = MutexGuard<'a, X>;
-}
-
-impl<T> std::hash::Hash for ArcMutex<T> {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        std::ptr::hash(&*self.0, state);
-    }
-}
-
-impl<T> std::fmt::Debug for ArcMutex<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "ArcMutex({:?})", self.0.as_ref() as *const _)
-    }
-}
-
-impl<T> PartialEq for ArcMutex<T> {
-    fn eq(&self, other: &Self) -> bool {
-        std::ptr::eq(&*self.0, &*other.0)
-    }
-}
-
-impl<T> Eq for ArcMutex<T> { }
-
-impl<T> Lock<T> for ArcMutex<T> {
-    fn borrow(&self) -> MutexGuard<'_, T> {
-        self.0.lock().unwrap()
-    }
-
-    fn borrow_mut(&self) -> MutexGuard<'_, T> {
-        self.0.lock().unwrap()
-    }
-}
-
-
-// This is unsafe and we sould allow it only with special compilation flag which marks caller
-// methods as unsafe.
-pub struct RawPtr<T>(*mut T);
-
-pub struct RawPtrSelector {}
-impl LockSelector for RawPtrSelector {
-    type Lock<T> = RawPtr<T>;
-    type Holder<T> = T;
-
-    fn new<T>(x: T) -> Self::Holder<T> {
-        x
-    }
-
-    fn refer<T>(x: &mut Self::Holder<T>) -> Self::Lock<T> {
-        RawPtr(x)
-    }
-
-    unsafe fn refer_uninit<T>(x: &mut Self::Holder<MaybeUninit<T>>) -> Self::Lock<T> {
-        RawPtr(x.as_mut_ptr())
-    }
-
-    fn borrow_mut_holder<T>(x: &mut T) -> &mut T {
-        x
-    }
-}
-
-impl<T> Clone for RawPtr<T> {
-    fn clone(&self) -> Self {
-        RawPtr(self.0)
-    }
-}
-
-impl<T> From<T> for RawPtr<T> {
-    fn from(x: T) -> Self {
-        RawPtr(Box::into_raw(Box::new(x)))
-    }
-}
-
-impl<T> std::fmt::Debug for RawPtr<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "RawPtr({:?})", self.0)
-    }
-}
-
-impl<T> std::hash::Hash for RawPtr<T> {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        std::ptr::hash(self.0, state);
-    }
-}
-
-impl<T> PartialEq for RawPtr<T> {
-    fn eq(&self, other: &Self) -> bool {
-        std::ptr::eq(self.0, other.0)
-    }
-}
-
-impl<T> Eq for RawPtr<T> { }
-
-impl<T> LockSuper for RawPtr<T> {
-    type Guard<'a, X: 'a> = &'a X;
-    type GuardMut<'a, X: 'a> = &'a mut X;
-}
-
-impl<T> Lock<T> for RawPtr<T> {
-    fn borrow(&self) -> &T {
-        unsafe { &*self.0 }
-    }
-
-    fn borrow_mut(&self) -> &mut T {
-        unsafe { &mut *self.0 }
-    }
-}
+// pub struct RcRefCell<T>(Option<Rc<RefCell<T>>>);
+// 
+// pub struct RcRefCellSelector {}
+// impl LockSelector for RcRefCellSelector {
+//     type Lock<T> = RcRefCell<T>;
+//     type Holder<T> = Rc<RefCell<T>>;
+// 
+//     fn empty<T>() -> Self::Lock<T> {
+//         RcRefCell(None)
+//     }
+// 
+//     fn new<T>(x: T) -> Self::Holder<T> {
+//         Rc::new(RefCell::new(x))
+//     }
+// 
+//     fn refer<T>(x: &mut Self::Holder<T>) -> Self::Lock<T> {
+//         RcRefCell(Some(x.clone()))
+//     }
+// 
+//     unsafe fn refer_uninit<T>(x: &mut Self::Holder<MaybeUninit<T>>) -> Self::Lock<T> {
+//         std::mem::transmute::<_, Self::Lock<T>>(x).clone()
+//     }
+// 
+//     fn borrow_mut_holder<T>(x: &mut Self::Holder<T>) -> RefMut<'_, T> {
+//         x.borrow_mut()
+//     }
+// }
+// 
+// impl<T> Clone for RcRefCell<T> {
+//     fn clone(&self) -> Self {
+//         RcRefCell(self.0.clone())
+//     }
+// }
+// 
+// impl<T> LockSuper for RcRefCell<T> {
+//     type Guard<'a, X: 'a> = Ref<'a, X>;
+//     type GuardMut<'a, X: 'a> = RefMut<'a, X>;
+// }
+// 
+// impl<T> Lock<T> for RcRefCell<T> {
+//     fn borrow(&self) -> Option<Ref<'_, T>> {
+//         self.0.as_ref().map(|x| x.borrow())
+//     }
+// 
+//     fn borrow_mut(&self) -> Option<RefMut<'_, T>> {
+//         self.0.as_ref().map(|x| x.borrow_mut())
+//     }
+// }
+// 
+// pub struct ArcMutex<T>(Option<Arc<Mutex<T>>>);
+// 
+// pub struct ArcMutexSelector {}
+// impl LockSelector for ArcMutexSelector {
+//     type Lock<T> = ArcMutex<T>;
+//     type Holder<T> = Arc<Mutex<T>>;
+// 
+//     fn empty<T>() -> Self::Lock<T> {
+//         ArcMutex(None)
+//     }
+// 
+//     fn new<T>(x: T) -> Self::Holder<T> {
+//         Arc::new(Mutex::new(x))
+//     }
+// 
+//     fn refer<T>(x: &mut Self::Holder<T>) -> Self::Lock<T> {
+//         ArcMutex(Some(x.clone()))
+//     }
+// 
+//     unsafe fn refer_uninit<T>(x: &mut Self::Holder<MaybeUninit<T>>) -> Self::Lock<T> {
+//         std::mem::transmute::<_, Self::Lock<T>>(x).clone()
+//     }
+// 
+//     fn borrow_mut_holder<T>(x: &mut Self::Holder<T>) -> MutexGuard<'_, T> {
+//         x.0.lock().unwrap()
+//     }
+// }
+// 
+// impl<T> From<T> for ArcMutex<T> {
+//     fn from(x: T) -> Self {
+//         ArcMutex(Arc::new(Mutex::new(x)))
+//     }
+// }
+// 
+// impl<T> Clone for ArcMutex<T> {
+//     fn clone(&self) -> Self {
+//         ArcMutex(self.0.clone())
+//     }
+// }
+// 
+// impl<T> LockSuper for ArcMutex<T> {
+//     type Guard<'a, X: 'a> = MutexGuard<'a, X>;
+//     type GuardMut<'a, X: 'a> = MutexGuard<'a, X>;
+// }
+// 
+// impl<T> std::hash::Hash for ArcMutex<T> {
+//     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+//         std::ptr::hash(&*self.0, state);
+//     }
+// }
+// 
+// impl<T> std::fmt::Debug for ArcMutex<T> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "ArcMutex({:?})", self.0.as_ref() as *const _)
+//     }
+// }
+// 
+// impl<T> PartialEq for ArcMutex<T> {
+//     fn eq(&self, other: &Self) -> bool {
+//         std::ptr::eq(&*self.0, &*other.0)
+//     }
+// }
+// 
+// impl<T> Eq for ArcMutex<T> { }
+// 
+// impl<T> Lock<T> for ArcMutex<T> {
+//     fn borrow(&self) -> MutexGuard<'_, T> {
+//         self.0.lock().unwrap()
+//     }
+// 
+//     fn borrow_mut(&self) -> MutexGuard<'_, T> {
+//         self.0.lock().unwrap()
+//     }
+// }
+// 
+// 
+// // This is unsafe and we sould allow it only with special compilation flag which marks caller
+// // methods as unsafe.
+// pub struct RawPtr<T>(*mut T);
+// 
+// pub struct RawPtrSelector {}
+// impl LockSelector for RawPtrSelector {
+//     type Lock<T> = RawPtr<T>;
+//     type Holder<T> = T;
+// 
+//     fn new<T>(x: T) -> Self::Holder<T> {
+//         x
+//     }
+// 
+//     fn refer<T>(x: &mut Self::Holder<T>) -> Self::Lock<T> {
+//         RawPtr(x)
+//     }
+// 
+//     unsafe fn refer_uninit<T>(x: &mut Self::Holder<MaybeUninit<T>>) -> Self::Lock<T> {
+//         RawPtr(x.as_mut_ptr())
+//     }
+// 
+//     fn borrow_mut_holder<T>(x: &mut T) -> &mut T {
+//         x
+//     }
+// }
+// 
+// impl<T> Clone for RawPtr<T> {
+//     fn clone(&self) -> Self {
+//         RawPtr(self.0)
+//     }
+// }
+// 
+// impl<T> std::fmt::Debug for RawPtr<T> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(f, "RawPtr({:?})", self.0)
+//     }
+// }
+// 
+// impl<T> std::hash::Hash for RawPtr<T> {
+//     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+//         std::ptr::hash(self.0, state);
+//     }
+// }
+// 
+// impl<T> PartialEq for RawPtr<T> {
+//     fn eq(&self, other: &Self) -> bool {
+//         std::ptr::eq(self.0, other.0)
+//     }
+// }
+// 
+// impl<T> Eq for RawPtr<T> { }
+// 
+// impl<T> LockSuper for RawPtr<T> {
+//     type Guard<'a, X: 'a> = &'a X;
+//     type GuardMut<'a, X: 'a> = &'a mut X;
+// }
+// 
+// impl<T> Lock<T> for RawPtr<T> {
+//     fn borrow(&self) -> &T {
+//         unsafe { &*self.0 }
+//     }
+// 
+//     fn borrow_mut(&self) -> &mut T {
+//         unsafe { &mut *self.0 }
+//     }
+// }
